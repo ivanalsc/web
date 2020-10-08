d647d7286352dcf6501ebbd1a688e2616186be08
ivanalsc <ivanasoscordero@gmail.com>
2020-10-08 11:20:25 -0300

    commit

---
+++ docs/precache-manifest.8950a6775c504ebd1c9f15701eede32b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48b393ca5bc9bdf3160fafbb1381dde",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "a24ef9c9b0297b6cb9da",
+    "url": "/web/static/css/main.aa2bfd0f.chunk.css"
+  },
+  {
+    "revision": "b72d9e72f42faf4c1e3a",
+    "url": "/web/static/js/2.875e57f1.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/web/static/js/2.875e57f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24ef9c9b0297b6cb9da",
+    "url": "/web/static/js/main.36a6cb40.chunk.js"
+  },
+  {
+    "revision": "40594163fae63f68d82a",
+    "url": "/web/static/js/runtime-main.3d4a80e6.js"
+  }
+]);
